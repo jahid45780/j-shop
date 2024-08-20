e0e449d6d73c1503a7d40f3c010f3967f929ed7f
jahid45780 <jahidhossain88899@gmail.com>
2024-08-20 22:40:43 +0600

    file

--- src/components/keen-slider/PicksSlider.jsx
+++ src/components/keen-slider/PicksSlider.jsx
@@ -2,6 +2,12 @@ import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 import "./styless.css"
 
+import photo1 from '../../../public/Picks/56fdea283c265fcf864774504d030448 1.jpg'
+import photo2 from '../../../public/Picks/7b6a1505c0d084e073873328a9fef520 3.jpg'
+import photo3 from '../../../public/Picks/aefff60654e4a164d9ed4590121f3d19 2.jpg'
+import photo4 from '../../../public/Picks/fe56bd0f218f4c6865ba14ec3461e4e 2.jpg'
+
+
 const PicksSlider = () => {
 
     const [sliderRef] = useKeenSlider({
@@ -16,16 +22,16 @@ const PicksSlider = () => {
 
 <div ref={sliderRef} className="keen-slider">
       <div className="keen-slider__slide number-slide1">
-        <img className=" w-full h-full" src="../../../public/Picks/56fdea283c265fcf864774504d030448 1.jpg" alt="" />
+        <img className=" w-full h-full" src={photo1} alt="" />
       </div>
       <div className="keen-slider__slide number-slide2">
-      <img className=" w-full h-full" src="../../../public/Picks/7b6a1505c0d084e073873328a9fef520 3.jpg" alt="" />
+      <img className=" w-full h-full" src={photo2} alt="" />
       </div>
       <div className="keen-slider__slide number-slide3">
-      <img className=" w-full h-full"  src="../../../public/Picks/aefff60654e4a164d9ed4590121f3d19 2.jpg" alt="" />
+      <img className=" w-full h-full"  src={photo3} alt="" />
       </div>
       <div className="keen-slider__slide number-slide4">
-      <img className=" w-full h-full"  src="../../../public/Picks/fe56bd0f218f4c6865ba14ec3461e4e 2.jpg" alt="" />
+      <img className=" w-full h-full"  src={photo4} alt="" />
       </div>
 
     </div>
--- src/components/keen-slider/useKeenSlider.jsx
+++ src/components/keen-slider/useKeenSlider.jsx
@@ -2,6 +2,12 @@ import React from "react"
 import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 import "./styless.css"
+import photo from '../../../public/Product/product1.jpg'
+import photo1 from '../../../public/Product/product2.jpg'
+import photo2 from '../../../public/Product/product3.jpg'
+import photo3 from '../../../public/Product/product4.jpg'
+
+
 
 export default () => {
   const [sliderRef] = useKeenSlider({
@@ -14,16 +20,16 @@ export default () => {
   return (
     <div ref={sliderRef} className="keen-slider">
       <div className="keen-slider__slide number-slide1">
-        <img className=" w-full h-full" src="../../../public/Product/product1.jpg" alt="" />
+        <img className=" w-full h-full" src={photo} alt="" />
       </div>
       <div className="keen-slider__slide number-slide2">
-      <img className=" w-full h-full" src="../../../public/Product/product2.jpg" alt="" />
+      <img className=" w-full h-full" src={photo1} alt="" />
       </div>
       <div className="keen-slider__slide number-slide3">
-      <img className=" w-full h-full"  src="../../../public/Product/product3.jpg" alt="" />
+      <img className=" w-full h-full"  src={photo2} alt="" />
       </div>
       <div className="keen-slider__slide number-slide4">
-      <img className=" w-full h-full"  src="../../../public/Product/product4.jpg" alt="" />
+      <img className=" w-full h-full"  src={photo3} alt="" />
       </div>
 
     </div>
